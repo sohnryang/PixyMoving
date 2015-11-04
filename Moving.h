2742652042112b7186989498004f37c1c515ca3d
sohnryang <ryang030528@gmail.com>
2015-11-04 17:50:42 +0900

    Create Moving.h

---
+++ Moving.h
@@ -0,0 +1,39 @@
+#ifndef __MOVING__
+#define __MOVING__
+
+#include <DCM.h>
+#include <MsTimer2.h>
+#include <Pixy.h>
+
+#define   DIR_FORWARD   0
+#define   DIR_LEFT      1
+#define   DIR_RIGHT     2
+#define   DIR_BACKWARD  3
+#define   DIR_LEFTTURN  4
+#define   DIR_RIGHTRURN 5
+
+DCM Motor1;
+DCM Motor2;
+DCM Motor3;
+DCM Motor4;
+
+void motorInit()
+{
+  Motor1.attachPins(16, 17);
+  Motor2.attachPins(18, 19);
+  Motor3.attachPins(20, 21);
+  Motor4.attachPins(22, 23);
+}
+
+void Move(int dir, int duration)
+{
+  if(dir == DIR_FORWARD)
+  {
+    Motor1.write(CW, 5);
+    Motor2.write(CCW, 5);
+    Motor3.write(CW, 5);
+    Motor4.write(CCW, 5);
+  }
+}
+
+#endif
