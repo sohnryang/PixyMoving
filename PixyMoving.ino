c376e8bf0de21055b8fa6afa03e14707754af07b
Sohn Ryang <ryang030528@gmail.com>
2015-11-18 17:32:36 +0900

    PixyMoving.ino changed
    Pixy test code added

--- PixyMoving.ino
+++ PixyMoving.ino
@@ -1,11 +1,42 @@
 #include "Moving.h"
 
+#include <SPI.h>  
+#include <Pixy.h>
+
+Pixy Camera;
+
 void setup() {
-//  pixy.init();
+  Serial.begin(9600);
+  Serial.print("Starting...\n");
+
+  Camera.init();
+  
   motorInit();
   Move(DIR_RIGHT, 10);
 }
 
 void loop() {
+  static int i = 0;
+  int j;
+  uint16_t blocks;
+  char buf[32]; 
 
+  blocks = pixy.getBlocks();
+
+  if (blocks)
+  {
+    i++;
+
+    if (i%50==0)
+    {
+      sprintf(buf, "Detected %d:\n", blocks);
+      Serial.print(buf);
+      for (j=0; j<blocks; j++)
+      {
+        sprintf(buf, "  block %d: ", j);
+        Serial.print(buf); 
+        pixy.blocks[j].print();
+      }
+    }
+  }  
 }
