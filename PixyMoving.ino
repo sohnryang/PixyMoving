4d2e0aa72cea17518d66a0d33c65233a047c47ac
Sohn Ryang <ryang030528@gmail.com>
2015-11-25 16:51:38 +0900

    PixyMoving.ino changed
    Tracking code added

--- PixyMoving.ino
+++ PixyMoving.ino
@@ -5,6 +5,8 @@
 
 Pixy Camera;
 
+void servoMove(int dir);
+
 void setup() {
   Serial.begin(9600);
   Serial.print("Starting...\n");
@@ -12,31 +14,27 @@ void setup() {
   Camera.init();
   
   motorInit();
-//  Move(DIR_RIGHT, 10);
 }
 
 void loop() {
   static int i = 0;
   int j;
   uint16_t blocks;
-  char buf[32]; 
+  char buf[32];
+  int32_t panError, tiltError;
 
-  blocks = Camera.getBlocks();
+  walkTime = millis();
+
+  blocks = pixy.getBlocks();
 
   if (blocks)
   {
-    i++;
+    panError = X_CENTER - pixy.blocks[0].x;                      //현재 상태에서 화면 중심을 기준으로 오차 계산
+    tiltError = pixy.blocks[0].y - Y_CENTER;
 
-    if (i%50==0)
-    {
-      sprintf(buf, "Detected %d:\n", blocks);
-      Serial.print(buf);
-      for (j=0; j<blocks; j++)
-      {
-        sprintf(buf, "  block %d: ", j);
-        Serial.print(buf);
-        Camera.blocks[j].print();
-      }
-    }
-  }  
+    panLoop.update(panError);                                    //2개의 모터(X,Y) 조작량 계산
+    tiltLoop.update(tiltError);
+
+    servoMove(panLoop.m_pos, tiltLoop.m_pos);                   //현재 계산된 위치에 따른 서보 각도 이동
+  }
 }
