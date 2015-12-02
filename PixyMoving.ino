1e0665ce691b36d95a145f3f70174d182023a104
Sohn Ryang <ryang030528@gmail.com>
2015-12-02 17:29:06 +0900

    PixyMoving.ino changed: Forwarding code added

--- PixyMoving.ino
+++ PixyMoving.ino
@@ -6,32 +6,40 @@
 Pixy Camera;
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  Camera.init();
+    Camera.init();
 
-  motorInit();
+    motorInit();
 }
 
 void loop() {
-  uint16_t blocks;
-  int blockPos;
-  int blockWidth;
-  int blockHeight;
+    uint16_t blocks;
+    int blockPos;
+    int blockWidth;
+    int blockHeight;
+    int blockArea;
 
-  blocks = Camera.getBlocks();
+    blocks = Camera.getBlocks();
 
-  if (blocks)
-  {
-    blockPos = Camera.blocks[0].x;
-    blockWidth = Camera.blocks[0].width;
-    blockHeight = Camera.blocks[0].height;
+    if (blocks)
+    {
+        blockPos = Camera.blocks[0].x;
+        blockWidth = Camera.blocks[0].width;
+        blockHeight = Camera.blocks[0].height;
+        blockArea = blockWidth * blockHeight;
 
-    if (blockPos < 50)
-      Move(DIR_LEFT);
-    else if (blockPos > 250)
-      Move(DIR_RIGHT);
-    else
-      Move(DIR_STOP);
-  }
+        if (blockPos < 50)
+            Move(DIR_LEFT);
+        else if (blockPos > 250)
+            Move(DIR_RIGHT);
+        else
+        {
+            Move(DIR_STOP);
+            if (blockArea < 20000)
+                Move(DIR_FORWARD);
+            else
+                Move(DIR_STOP);
+        }
+    }
 }
