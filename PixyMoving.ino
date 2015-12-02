09bf97e5e7b80d1cd1899cad5c275d1c5390258e
Sohn Ryang <ryang030528@gmail.com>
2015-12-02 17:08:10 +0900

    PixyMoving.ino changed Added block size calculating code.

--- PixyMoving.ino
+++ PixyMoving.ino
@@ -1,6 +1,6 @@
 #include "Moving.h"
 
-#include <SPI.h>  
+#include <SPI.h>
 #include <Pixy.h>
 
 Pixy Camera;
@@ -9,21 +9,25 @@ void servoMove(int dir);
 
 void setup() {
   Serial.begin(9600);
-  
+
   Camera.init();
-  
+
   motorInit();
 }
 
 void loop() {
   uint16_t blocks;
   int blockPos;
+  int blockWidth;
+  int blockHeight;
 
   blocks = Camera.getBlocks();
 
   if (blocks)
   {
     blockPos = Camera.blocks[0].x;
+    blockWidth = Camera.blocks[0].width;
+    blockHeight = Camera.blocks[0].height;
 
     if (blockPos < 50)
       Move(DIR_LEFT);
